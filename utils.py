74d8845186dcea551168d168cbda3a81b0883b85
Shravan <shravan.chandrashekharaiah@gmail.com>
2021-07-13 11:49:42 +0100

    comments

--- utils.py
+++ utils.py
@@ -6,9 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 GENUINE_DOMAIN_PATH = '../datasets/genuine_domains/'
 MALWARE_DOMAIN_PATH = '../datasets/malware_domains/'
+
+# Dictionary for allowed characters in domain
 domain_name_dictionary = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, ':': 10,
                           '-': 11, '.': 12, '/': 13, '_': 14, 'a': 15, 'b': 16, 'c': 17, 'd': 18, 'e': 19, 'f': 20,
                           'g': 21, 'h': 22, 'i': 23, 'j': 24, 'k': 25, 'l': 26, 'm': 27, 'n': 28, 'o': 29, 'p': 30,
@@ -17,19 +18,37 @@ domain_name_dictionary = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6
 
 
 def domain_to_ints(domain):
+    """
+    example:
+    input: domain="abc"
+    return: [15, 16, 17]
+    """
     return [
         domain_name_dictionary.get(y, domain_name_dictionary.get(np.NaN))
         for y in domain.lower()
     ]
 
 
-def prep_data(data, max_length):
+def prep_data(data, max_length=10):
+    """
+    Used while predicting
+    example:
+    input: "abc", max_length=10
+    return: [
+                [ 0  0  0  0  0  0  0  0  0 15]
+                [ 0  0  0  0  0  0  0  0  0 16]
+                [ 0  0  0  0  0  0  0  0  0 17]
+            ]
+    """
     return sequence.pad_sequences(
         np.array([domain_to_ints(x) for x in data]), maxlen=max_length
     )
 
 
 def prep_dataframe(data, max_length=255):
+    """
+    Returns "X" converting each domain to numerical sequence and label numpy array
+    """
     X = (data["domain"]
          .apply(lambda x: domain_to_ints(x))
          .pipe(sequence.pad_sequences, maxlen=max_length))
@@ -37,7 +56,11 @@ def prep_dataframe(data, max_length=255):
     return X, y.to_numpy()
 
 
-def get_and_split_data(max_length):
+def get_and_split_data(max_length=255):
+    """
+    Loop over genuine domain file lists and malware domain file lists
+    returns training set and validation set
+    """
     df = pd.DataFrame()
     for name in glob.glob(GENUINE_DOMAIN_PATH + '*'):
         temp = pd.read_csv(name, names=['domain', 'flag'])
@@ -57,6 +80,9 @@ def get_and_split_data(max_length):
 
 
 def prepare_dataset(data, labels, batch=32, shuffle_buffer=50):
+    """
+    Converts numpy array into tf.data type(scalable used on GCP)
+    """
     dataset = tf.data.Dataset.from_tensor_slices((data, labels))
     dataset = dataset.shuffle(shuffle_buffer)
     dataset = dataset.batch(batch).prefetch(1)
@@ -91,3 +117,5 @@ def plot_curve(history):
     plt.plot(epochs, val_loss, label=['Validation Loss'])
     plt.legend()
     plt.show()
+
+print(prep_data("abc"))
